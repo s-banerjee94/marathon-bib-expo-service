8d2b3cfc55c1feca88d017def30bfea6c34c74b9
s-banerjee94 <shondha.tara94@gmail.com>
2025-12-21 12:34:20 +0530

    Initialize Spring Boot project with Maven wrapper, basic configurations, and dependencies.

---
+++ src/main/java/com/timekeeper/bibexpo/MarathonBibExpoApplication.java
@@ -0,0 +1,13 @@
+package com.timekeeper.bibexpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarathonBibExpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarathonBibExpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timekeeper/bibexpo/MarathonBibExpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timekeeper.bibexpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarathonBibExpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
