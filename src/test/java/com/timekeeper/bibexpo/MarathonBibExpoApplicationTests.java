d1055e82fed710a6129e40415dc29689d5bb03f5
s-banerjee94 <shondha.tara94@gmail.com>
2026-04-19 22:03:50 +0530

    test: remove empty contextLoads test from application tests

--- src/test/java/com/timekeeper/bibexpo/MarathonBibExpoApplicationTests.java
+++ src/test/java/com/timekeeper/bibexpo/MarathonBibExpoApplicationTests.java
@@ -1,13 +1,8 @@
 package com.timekeeper.bibexpo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MarathonBibExpoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
